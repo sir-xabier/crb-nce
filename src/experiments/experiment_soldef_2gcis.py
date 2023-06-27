cd7d3ecfdbc6fe283f5be65199ba411d4bbc5481
sir-xabier <dataxabiens@gmail.com>
2023-06-27 14:40:20 +0200

    New Framework

--- src/experiments/experiment_soldef_2gcis.py
+++ src/experiments/experiment_soldef_2gcis.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 11 12:13:23 2022
 
-@author: user
+Experimento para la detección del número de clusters, comparando GCI45 y 50 con las otras medidas.
+Incluye MSE además de Acc y MAE.
+Incluye test estadísticos para las 3 métricas.
+Incluye construcción de gráficos de la evolución de las 3 métricas en función de std.
+
+FALTARIA:
+Análisis de resultados para K=1 obteniendo matrices de confusión binarias y calculando métricas habituales 
+    (especificidad, sensibilidad, precisión, F, etc.) -- Solo para BIC, VLR y GCI45 y 50
+Gráficos de evolución de métricas en función de K y D.
+Análisis de sensibilidad del resultado en función de k_max. Para un máximo de k_max=35 se puede hacer a partir de los csv
+    que se importan al principio del código (los tail ratios se calculan en utils -> conds_score8)
+
 """
 
 import os
@@ -51,7 +61,7 @@ u=np.zeros(ru)
 c=np.zeros(rc-ru)
 b=np.zeros(2)
 
-id_sol="soldef_2gci"
+id_sol="soldef_2gci_vdef"
 
 #umbrales
 u[0]=0; u[1]=0; u[2]=0.97; u[3]=0; u[4]=0; u[5]=2.2; u[6]=0; u[7]=4;
@@ -67,19 +77,24 @@ all_df={"s":df_s,"ch":df_ch,"db":df_db,"bic":df_bic,"curv_m":df_cm,"xie_b":df_xb
         "y":df_y}
 crit=["s","ch","db", "bic", "curv_m", "xie_b", "var_lr"]
 crit_gci=["gci45","gci50"]
+#crit_gci=["gci45"]
 col_crit=crit+crit_gci
 df=pd.DataFrame(columns=col_crit.copy().append("y"),index=df_s.index)
 df_flag=pd.DataFrame(columns=crit_gci,index=df_s.index)
 
-select_k= lambda x: np.nanargmax(x)+1
-select_k_db= lambda x: np.nanargmin(x)+1
+select_k_max= lambda x: np.nanargmax(x)+1
+select_k_min= lambda x: np.nanargmin(x)+1
+#select_k_vlr = lambda x: np.nanargmin(np.abs(x - 1)) +1
+select_k_vlr = lambda x: np.amax(np.array(x <= 1.0).nonzero())+1
 
 for name,df_ in all_df.items():
   if name!="y" and ("gci" in name)==False:
-    if name=="db":
-      df[name] = df_.apply(select_k_db,axis=1).values.reshape(-1,1)
+    if name=="db" or name=="xie_b":
+      df[name] = df_.apply(select_k_min,axis=1).values.reshape(-1,1)
+    elif name=="var_lr":
+      df[name] = df_.apply(select_k_vlr,axis=1).values.reshape(-1,1)
     else:
-      df[name] = df_.apply(select_k,axis=1).values.reshape(-1,1)
+      df[name] = df_.apply(select_k_max,axis=1).values.reshape(-1,1)
   elif "gci" in name:
     pred_flag=df_.apply(lambda x: conds_score8(gci_=x,id=x.name,**args),axis=1).values
     flags=[]
@@ -100,7 +115,7 @@ df=df[no_cmeans]
 df.drop(columns=["algorithm"],axis=1,inplace=True)
 
 
-df_metrics=pd.DataFrame(columns=col_crit,index=["MAE","ACC"])
+df_metrics=pd.DataFrame(columns=col_crit,index=["MAE","MSE","ACC"])
 #Global metrics
 for c in df.columns[:-1]:
     df_metrics[c]= [mean_absolute_error(df[c],df["y"]),mean_squared_error(df[c],df["y"]),accuracy_score(df[c],df["y"])]
@@ -122,7 +137,8 @@ df_["K"]=df_.apply(lambda x: "Control" if "blobs-" not in x.name else x.dataset.
 df_["dt"]=df_.apply(lambda x: "Control" if "blobs-" not in x.name else x.dataset.split("-")[-1],axis=1)
 df_["flags"]=np.asarray(flags)[no_cmeans]
 
-acc= lambda x: len(np.where(x==0)[0])/len(x)
+acc = lambda x: len(np.where(x==0)[0])/len(x)
+mse = lambda x: np.mean(np.square(np.array(x)))
 
 df_=df_[df_["algorithm"]!="cmeans"]
 
@@ -142,6 +158,14 @@ df_.drop(columns=["dataset","algorithm","scenario"],axis=1).groupby(["dimensions
 df_.drop(columns=["dataset","algorithm","scenario","dt"],axis=1).groupby(["dimensions"]).agg(acc).to_excel(ROOT+"/out_files/F_acc_dim_"+id_sol+".xlsx")
 df_.drop(columns=["dataset","algorithm","scenario","dt","dimensions"],axis=1).groupby(["flags"]).agg(acc).to_excel(ROOT+"/out_files/F_acc_flag_"+id_sol+".xlsx")
 
+df_.drop(columns=["dataset","dimensions","N","scenario","K","dt"],axis=1).groupby("algorithm").agg(mse).to_excel(ROOT+"/out_files/F_mse_algo_"+id_sol+".xlsx")
+df_.drop(columns=["dataset","dimensions","N","K","dt"],axis=1).groupby(["scenario","algorithm"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_scen_algo_"+id_sol+".xlsx")
+df_.drop(columns=["dataset","dimensions","N","algorithm","K","dt"],axis=1).groupby(["scenario"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_scen_"+id_sol+".xlsx")
+df_.drop(columns=["dataset","dimensions","N","algorithm","scenario","K"],axis=1).groupby(["dt"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_dt_"+id_sol+".xlsx")
+df_.drop(columns=["dataset","algorithm","scenario","N","K"],axis=1).groupby(["dimensions","dt"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_dim_dt_"+id_sol+".xlsx")
+df_.drop(columns=["dataset","algorithm","scenario","dt","N","K"],axis=1).groupby(["dimensions"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_dim_"+id_sol+".xlsx")
+df_.drop(columns=["dataset","algorithm","scenario","dt","dimensions","N","K"],axis=1).groupby(["flags"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_flag_"+id_sol+".xlsx")
+
 df_.to_excel(ROOT+"/out_files/F_err_"+id_sol+".xlsx")
 df.to_excel(ROOT+"/out_files/F_"+id_sol+".xlsx")
 df_metrics.to_excel(ROOT+"/out_files/F_metrics_"+id_sol+".xlsx")
@@ -153,6 +177,13 @@ df1.drop(columns=["N","K","scenario"],axis=1).groupby(["dimensions"]).agg(acc).t
 
 df1.drop(columns=["dimensions","N","K"],axis=1).groupby(["scenario"]).mean().to_excel(ROOT+"/out_files/F_K1_mean_scen_"+id_sol+".xlsx")
 df1.drop(columns=["dimensions","N","K","scenario"],axis=1).mean().to_excel(ROOT+"/out_files/F_K1_mean_"+id_sol+".xlsx")
+df1.drop(columns=["N","K","scenario"],axis=1).groupby(["dimensions"]).mean().to_excel(ROOT+"/out_files/F_K1_mean_dim_"+id_sol+".xlsx")
+
+df1.drop(columns=["dataset","dimensions","N","algorithm","K","dt"],axis=1).groupby(["scenario"]).agg(mse).to_excel(ROOT+"/out_files/F_K1_mse_scen_"+id_sol+".xlsx")
+df1.drop(columns=["dataset","algorithm","scenario","dt","dimensions","N","K"],axis=1).agg(mse).to_excel(ROOT+"/out_files/F_K1_mse_"+id_sol+".xlsx")
+df1.drop(columns=["dataset","algorithm","scenario","dt","N","K"],axis=1).groupby(["dimensions"]).agg(mse).to_excel(ROOT+"/out_files/F_K1_mse_dim_"+id_sol+".xlsx")
+
+
 
 dfn1=df_[df_["K"]!="K1"]
 dfn1.drop(columns=["dataset","algorithm"],axis=1).mean().to_excel(ROOT+"/out_files/F_SinK1_mean_"+id_sol+".xlsx")
@@ -163,7 +194,7 @@ dfn1.drop(columns=["dataset","algorithm","scenario"],axis=1).groupby(["dt"]).mea
 dfn1.drop(columns=["dataset","algorithm","scenario"],axis=1).groupby(["dimensions","dt"]).mean().to_excel(ROOT+"/out_files/F_SinK1_mean_dim_dt_"+id_sol+".xlsx")
 dfn1.drop(columns=["dataset","algorithm","scenario","dt"],axis=1).groupby(["dimensions"]).mean().to_excel(ROOT+"/out_files/F_SinK1_mean_dim_"+id_sol+".xlsx")
 dfn1.drop(columns=["dataset","algorithm","scenario","dt","dimensions"],axis=1).groupby(["flags"]).mean().to_excel(ROOT+"/out_files/F_SinK1_mean_flag_"+id_sol+".xlsx")
-
+dfn1.drop(columns=["dataset","algorithm","K"],axis=1).groupby(["scenario","flags"]).mean().to_excel(ROOT+"/out_files/F_SinK1_meanc_scen_flag_"+id_sol+".xlsx")
 
 dfn1.drop(columns=["dataset","algorithm"],axis=1).agg(acc).to_excel(ROOT+"/out_files/F_SinK1_acc_"+id_sol+".xlsx")
 dfn1.drop(columns=["scenario","dataset"],axis=1).groupby(["algorithm"]).agg(acc).to_excel(ROOT+"/out_files/F_SinK1_acc_algo_"+id_sol+".xlsx")
@@ -175,15 +206,29 @@ dfn1.drop(columns=["dataset","algorithm","scenario","dt"],axis=1).groupby(["dime
 dfn1.drop(columns=["dataset","algorithm","scenario","dt","dimensions"],axis=1).groupby(["flags"]).agg(acc).to_excel(ROOT+"/out_files/F_SinK1_acc_flag_"+id_sol+".xlsx")
 dfn1.drop(columns=["dataset","algorithm","K"],axis=1).groupby(["scenario","flags"]).agg(acc).to_excel(ROOT+"/out_files/F_SinK1_acc_scen_flag_"+id_sol+".xlsx")
 
+dfn1.drop(columns=["dataset","algorithm","scenario","dt","dimensions","N","K"],axis=1).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","scenario","dt","dimensions","N","K"],axis=1).groupby(["algorithm"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_algo_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","dt","dimensions","N","K"],axis=1).groupby(["scenario","algorithm"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_scen_algo_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","algorithm","dt","dimensions","N","K"],axis=1).groupby(["scenario"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_scen_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","algorithm","scenario","dimensions","N","K"],axis=1).groupby(["dt"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_dt_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","algorithm","scenario","N","K"],axis=1).groupby(["dimensions","dt"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_dim_dt_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","algorithm","scenario","dt","N","K"],axis=1).groupby(["dimensions"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_dim_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","algorithm","scenario","dt","dimensions","N","K"],axis=1).groupby(["flags"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_flag_"+id_sol+".xlsx")
+dfn1.drop(columns=["dataset","algorithm","dt","dimensions","N","K"],axis=1).groupby(["scenario","flags"]).agg(mse).to_excel(ROOT+"/out_files/F_SinK1_mse_scen_flag_"+id_sol+".xlsx")
+
 
 dfc=df_[df_["scenario"]=="Control\n"]
 dfc.drop(columns=["dimensions","N","algorithm","K","scenario","dt","flags"],axis=1).groupby(["dataset"]).agg(acc).to_excel(ROOT+"/out_files/F_acc_control_"+id_sol+".xlsx")
 dfc.drop(columns=["dimensions","N","algorithm","K","scenario","dt","flags"],axis=1).groupby(["dataset"]).mean().to_excel(ROOT+"/out_files/F_mean_control_"+id_sol+".xlsx")
+dfc.drop(columns=["algorithm","scenario","dt","dimensions","N","K","flags"],axis=1).groupby(["dataset"]).agg(mse).to_excel(ROOT+"/out_files/F_mse_control_"+id_sol+".xlsx")
 
 
 out=dfn1.drop(columns=["dataset","algorithm","K"],axis=1).groupby(["scenario"]).agg(acc)
-out=out.drop(columns=["dimensions","N","dt"],axis=1)
-iman_davenport, p_value, rankings_avg, rankings_cmp=friedman_test(out.s, out.ch, out.db, out.gci45, out.gci50)
+out=out.drop(columns=["dimensions","N","dt","flags"],axis=1)
+iman_davenport, p_value, rankings_avg, rankings_cmp=friedman_test(out.s, out.ch, out.db, out.bic, out.curv_m, out.xie_b, out.var_lr,
+                                                                  out.gci45, out.gci50)
+print("Acc",rankings_avg)
+
 if p_value < 0.05:
     control=np.argmax(rankings_avg)
     dic={}
@@ -195,8 +240,11 @@ if p_value < 0.05:
     
     
 out=dfn1.drop(columns=["dataset","algorithm","K"],axis=1).groupby(["scenario"]).mean()
-#out=out.drop(columns=["dimensions","N","dt"],axis=1)
-iman_davenport, p_value, rankings_avg, rankings_cmp=friedman_test(out.s, out.ch, out.db, out.gci45, out.gci50)
+out=out.drop(columns=["flags"],axis=1)
+iman_davenport, p_value, rankings_avg, rankings_cmp=friedman_test(out.s, out.ch, out.db, out.bic, out.curv_m, out.xie_b, out.var_lr,
+                                                                  out.gci45, out.gci50)
+print("MAE",rankings_avg)
+
 if p_value < 0.05:
     control=np.argmin(rankings_avg)
     dic={}
@@ -205,7 +253,22 @@ if p_value < 0.05:
     comparisons, z_values, p_values, adj_p_values=holm_test(dic,control=col_crit[control])
     data={'z_value':z_values,'p_value':p_values,'adj_p_value':adj_p_values}
     df_holm=pd.DataFrame(data,index=comparisons).to_excel(ROOT+"/out_files/F_Holm_mean"+id_sol+".xlsx")
-    
+
+
+out=dfn1.drop(columns=["dimensions","N","dt","flags"],axis=1)
+out=out.drop(columns=["dataset","algorithm","K"],axis=1).groupby(["scenario"]).agg(mse)
+iman_davenport, p_value, rankings_avg, rankings_cmp=friedman_test(out.s, out.ch, out.db, out.bic, out.curv_m, out.xie_b, out.var_lr,
+                                                                  out.gci45, out.gci50)
+print("MSE",rankings_avg)
+
+if p_value < 0.05:
+    control=np.argmin(rankings_avg)
+    dic={}
+    for i,key in enumerate(col_crit):
+        dic[key]=rankings_cmp[i] 
+    comparisons, z_values, p_values, adj_p_values=holm_test(dic,control=col_crit[control])
+    data={'z_value':z_values,'p_value':p_values,'adj_p_value':adj_p_values}
+    df_holm=pd.DataFrame(data,index=comparisons).to_excel(ROOT+"/out_files/F_Holm_mse"+id_sol+".xlsx")
     '''
 df_["flags"].sum()
 (1-df_["flags"]).sum()
@@ -263,6 +326,24 @@ dfn1_plot_dt.index.rename('dt', inplace=True)
 sns.relplot(
     data=dfn1_plot_dt,kind="line",
     x="dt", y="MAE",
+    hue="Method"
+)
+
+dfn1p=dfn1[dfn1["dt"]!="Control"]
+dfn1p["dt"]=[float(dt[2:]) for dt in dfn1p["dt"]]
+dfn1_plot_dt=pd.DataFrame(columns=["MSE","Method"])
+for met in col_crit:
+    dfn1_aux=dfn1p[[met,"dt"]].groupby(["dt"]).agg(mse).rename(columns={met:"MSE"})
+    ma=moving_average(np.asarray(dfn1_aux["MSE"]),n=n)
+    dfn1_aux=pd.DataFrame(ma,columns=["MSE"],index=dfn1_aux.index[n-1:])
+    dfn1_aux["Method"]=met
+    dfn1_plot_dt=pd.concat([dfn1_plot_dt,dfn1_aux],axis=0)
+
+dfn1_plot_dt.index.rename('dt', inplace=True)
+
+sns.relplot(
+    data=dfn1_plot_dt,kind="line",
+    x="dt", y="MSE",
     hue="Method"
 )
 
