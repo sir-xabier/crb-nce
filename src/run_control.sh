d832ff6bf0dc9bd7819fcf3c5dfcffbd1c476308
sir-xabier <dataxabiens@gmail.com>
2025-02-19 16:05:07 +0100

    run test fixed 3

--- src/run_control.sh
+++ src/run_control.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Define parameter ranges
+# Define the list of ICVIs (Internal Cluster Validity Indices)
 ICVIS=("reval" "s" "ch" "db" "sse" "vlr" "bic" "xb" "gci" "gci2" "cv")
 
 # Iterate over datasets and ICVIs
 for folder in "datasets/control"; do
-    for dataset in "./$folder"/*; do
+    for dataset in "$folder"/*; do
         for icvi in "${ICVIS[@]}"; do
             # Submit task to tsp queue
             tsp python src/data/experiment_control.py \
-                -dataset "${dataset#.}" \
-                -icvi "$icvi" 
+                -dataset "$dataset" \
+                -icvi "$icvi" \
+                --seed 31416 \
+                --n_init 10 \
+                --kmax 50 \
+                --maxiter 100
         done
     done
 done
